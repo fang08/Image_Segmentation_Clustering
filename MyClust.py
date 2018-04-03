8b8b4e12883660bf100d612664701a177382e7ae
Arslan Siddiqui <monker490@github.com>
2018-04-02 21:40:58 -0400

    SOM added

--- MyClust.py
+++ MyClust.py
@@ -1,12 +1,9 @@
 import numpy as np
 import scipy.io as sio
 import matplotlib.pyplot as plt
-<<<<<<< HEAD
 from skimage import morphology
 
-=======
 from MyClustEvalRGB import evalRGB
->>>>>>> 4c6e954f1c339c88477b18eecbecbbe8a6140660
 from sklearn.cluster import KMeans
 from sklearn.cluster import SpectralClustering
 from sklearn.mixture import GaussianMixture
@@ -16,11 +13,12 @@ from scipy.ndimage import gaussian_filter
 
 
 from MyKmeans import MyKmeans
+from MySOM import MySOM
 # from MyGMM import MyGMM
 # from MyFCM import MyFCM
 
 ## load RGB images
-mat_rgb = sio.loadmat('ProjectFiles/ImsAndSegs/ImsAndTruths2092.mat')  # could let user input name of picture
+mat_rgb = sio.loadmat('ImsAndTruths2092.mat')  # could let user input name of picture
 im_rgb = mat_rgb['Im']
 seg1 = mat_rgb['Seg1']
 seg2 = mat_rgb['Seg2']
@@ -30,8 +28,8 @@ plt.show()
 
 
 # load hyperspectral images
-# mat_hyper = sio.loadmat('PaviaHyperIm.mat')   # another one santa barbara
-# im_pavia = mat_hyper['PaviaHyperIm']
+mat_hyper = sio.loadmat('PaviaHyperIm.mat')   # another one santa barbara
+im_pavia = mat_hyper['PaviaHyperIm']
 
 # for i in range(len(result)):
 #     result[i] = [k + 1 for k in result[i]]
@@ -44,24 +42,24 @@ plt.show()
 # plt.colorbar()
 # plt.show()
 
-plt.imshow(seg2)
-plt.colorbar()
-plt.show()
+#plt.imshow(seg2)
+#plt.colorbar()
+#plt.show()
 
-seg2 = relabel(seg2)
-plt.imshow(seg2)
-plt.colorbar()
-plt.show()
+#seg2 = relabel(seg2)
+#plt.imshow(seg2)
+#plt.colorbar()
+#plt.show()
 
-result2 = MyKmeans(im_rgb, 'RGB', 10)
-plt.imshow(result2)
-plt.colorbar()
-plt.show()
+#result2 = MyKmeans(im_rgb, 'RGB', 10)
+#plt.imshow(result2)
+#plt.colorbar()
+#plt.show()
 
-result2 = relabel(gaussian_filter(result2, sigma=3))
-plt.imshow(result2)
-plt.colorbar()
-plt.show()
+#result2 = relabel(gaussian_filter(result2, sigma=3))
+#plt.imshow(result2)
+#plt.colorbar()
+#plt.show()
 
 
 # plt.imshow(seg3)
@@ -74,7 +72,7 @@ plt.show()
 # plt.show()
 
 # print(evalRGB(result1, seg1))
-print(evalRGB(result2, seg2))
+# print(evalRGB(result2, seg2))
 # print(evalRGB(result3, seg3))
 
 # result = MyKmeans(im_pavia, 'Hyper', 8)
@@ -92,20 +90,18 @@ print(evalRGB(result2, seg2))
 # plt.colorbar()
 # plt.show()
 
-<<<<<<< HEAD
-result = MyFCM(im_rgb, 'RGB', 8)
-con_comp = morphology.label(result, 8)
-plt.figure(1)
-plt.subplot(121)
-plt.title('result')
-plt.imshow(result)
 
-plt.subplot(122)
-plt.title('connected components')
-plt.imshow(con_comp)
-
-plt.tight_layout()
-plt.show()
+#result = MyFCM(im_rgb, 'RGB', 8)
+#con_comp = morphology.label(result, 8)
+#plt.figure(1)
+#plt.subplot(121)
+#plt.title('result')
+#plt.imshow(result)
+#plt.subplot(122)
+#plt.title('connected components')
+#plt.imshow(con_comp)
+#plt.tight_layout()
+#plt.show()
 
 #result = MyFCM(im_pavia, 'Hyper', 8)
 #plt.imshow(result)
@@ -116,14 +112,9 @@ plt.show()
 #plt.imshow(result)
 #plt.colorbar()
 #plt.show()
-=======
-# result = MyFCM(im_rgb, 'RGB', 8)
-# plt.imshow(result)
-# plt.colorbar()
-# plt.show()
 
-# result = MyFCM(im_pavia, 'Hyper', 8)
-# plt.imshow(result)
-# plt.colorbar()
-# plt.show()
->>>>>>> 4c6e954f1c339c88477b18eecbecbbe8a6140660
+result = MySOM(im_pavia, 'Hyper', 8)
+plt.imshow(result)
+plt.colorbar()
+plt.show()
+
