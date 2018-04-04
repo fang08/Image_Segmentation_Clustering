e1f9b98d1e2d77175bf3d74886ad36070b7ea98a
cjnbill <cjnseu@gmail.com>
2018-04-04 17:29:58 -0400

    eval santabar hyper

--- MyEvalSb.py
+++ MyEvalSb.py
@@ -1,14 +1,18 @@
-import MyKmeans,MyFCM,MyGMM,MySOM,MySpectral
+from MyKmeans import MyKmeans
+from MyFCM import MyFCM
+from MyGMM import MyGMM
+from MySOM import MySOM
+from MySpectral import MySpectral
 from scipy.io import loadmat,savemat
 
 if __name__ == '__main__':
     mat = loadmat('SanBarHyperIm.mat')
     im=mat["SanBarIm88x400"]
-    km = MyKmeans.MyKmeans(im,"RGB",5)
-    fcm = MyFCM.MyFCM(im,"RGB",5)
-    gmm = MyGMM.MyGMM(im,"RGB",5)
-    som = MySOM.MySOM(im,"RGB",5)
-    sc = MySpectral.MySpectral(im,"RGB",5)
+    km = MyKmeans(im,"RGB",5)
+    fcm = MyFCM(im,"RGB",5)
+    gmm = MyGMM(im,"RGB",5)
+    som = MySOM(im,"RGB",5)
+    sc = MySpectral(im,"RGB",5)
     savemat("km.mat",km)
     savemat("fcm.mat",fcm)
     savemat("gmm.mat",gmm)
