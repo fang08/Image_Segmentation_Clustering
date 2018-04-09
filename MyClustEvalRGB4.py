fc6067372eb8d3ceae89969f0a42ebe75aaa4590
cjnbill <cjnseu@gmail.com>
2018-04-09 00:37:25 -0400

    add args

--- MyClustEvalRGB4.py
+++ MyClustEvalRGB4.py
@@ -1,4 +1,17 @@
 from MyMartinIndex4 import oce
+from scipy.io import loadmat
+import sys
 
 def evalRGB(seg, gt):
     return oce(seg, gt)
+
+
+
+if __name__ == '__main__':
+    f1=sys.argv[1]
+    f2=sys.argv[2]
+
+    gt = loadmat(f1)['res']
+    seg = loadmat(f2)['res']
+
+    print(evalRGB(seg, gt))
